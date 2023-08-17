1afdc9cfbe71074b021c84a702d2cb4303ae9128
Dru Kepple <dru@fittedrunning.com>
2023-08-17 13:40:10 -0600

    wip main page

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Peter Garbriel’s I/O</h1>
+      <p>Explore the various mixes of each song</p>
+      <div></div>
+    </main>
+  )
+}
